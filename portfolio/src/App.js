7b0f7f9d3479ac315a339cc889487c486cdc3530
ah1096 <alyssa.holbert@student.awesomeinc.org>
2023-01-06 15:53:01 +0000

    added remaining files from previous site

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar.js';
+import AboutMe from './AboutMe.js';
+import Projects from './Projects.js';
+import ContactMe from './ContactMe.js'
+import Footer from './Footer.js';
 
 function App() {
   return (
