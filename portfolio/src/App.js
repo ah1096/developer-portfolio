5a5b16f2f1fe994b836dde6262a6a2fa6604eb18
ah1096 <arh.10.96@gmail.com>
2023-04-17 15:51:24 +0000

    add basic structure notes from wireframe

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 
 function App() {
 
+//LANDING PAGE: brand statement + icon
 
+//PROJECT PAGE: top 3 projects as cards w/ hover effect for descriptions.
 
+//ABOUT PAGE: blurb about my education, professional experience, interests + photo; tech stack list
 
+//CONTACT PAGE: blurb with CTA, icons w/ links to socials, email form
 
-
-
-  
 }
 
 export default App;
