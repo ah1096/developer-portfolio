35a36c86dd663f32e7e2e37d8ab7c582ef0d1a9c
ah1096 <arh.10.96@gmail.com>
2023-04-19 17:49:10 +0000

    AboutPage styling

--- portfolio/src/Components/AboutPage.js
+++ portfolio/src/Components/AboutPage.js
@@ -1,16 +1,27 @@
+import logo from '../images/AH_coding_logo.png'
+const brandLogo = {
+    name: 'brandLogo',
+    imageUrl: logo,
+    imageHeight: 90,
+    imageWidth: 90,
+};
+
 
 export const AboutPage = () => {
     return ( 
-        <div id="aboutpage" className="container">
+        <div id="aboutpage" className="container vh-100 w-100 mx-auto">
             <h1>About me</h1>
 
-            <div className="row">
-                <div id="abouttext" className="col-8">
+            <div className="row align-items-center">
+                <div id="abouttext" className="col-9">
                     <p>this is a blurb with some texts about my education, experience, and interests</p>
                 </div>
 
-                <div id="profilepic" className="col-4">
-                    <h1>profile pic goes here</h1>
+                <div id="profilepic" className="col-3">
+                    <img className="w-100" src={brandLogo.imageUrl} 
+                    alt="a dark pink hexagon inside with is Alyssa Holbert's brand logo, 
+                    a small letter H inside of a large letter A; boths letters share a 
+                    horizontal line" />
                 </div>
             </div>
 
@@ -27,29 +38,35 @@ export const AboutPage = () => {
 
                 <div className="col-4" id="languages">
                     <h10>languages</h10>
-                    <ul id="iconlist">
-                        <li>HTML + CSS</li>
-                        <li>Javascript</li>
-                        <li>Python</li>
-                    </ul>
+                    <div className="iconlist">
+                        <ul>
+                            <li>HTML + CSS</li>
+                            <li>Javascript</li>
+                            <li>Python</li>
+                        </ul>
+                    </div>
                 </div>
 
                 <div className="col-4" id="frameworks">
                     <h10>frameworks and libraries</h10>
-                    <ul id="iconlist">
-                        <li>React</li>
-                        <li>Bootstrap</li>
-                        <li>Django</li>
-                    </ul>
+                    <div className="iconlist">
+                        <ul>
+                            <li>React</li>
+                            <li>Bootstrap</li>
+                            <li>Django</li>
+                        </ul>
+                    </div>
                 </div>
 
                 <div className="col-4" id="otherskills">
                     <h10>other skills + tools</h10>
-                    <ul id="iconlist">
-                        <li>Github CLI</li>
-                        <li>Agile Methodology</li>
-                        <li>Communication</li>
-                    </ul>
+                    <div className="iconlist">
+                        <ul>
+                            <li>Github CLI</li>
+                            <li>Agile Methodology</li>
+                            <li>API Integration</li>
+                        </ul>
+                    </div>
                 </div>
 
                 </div>
