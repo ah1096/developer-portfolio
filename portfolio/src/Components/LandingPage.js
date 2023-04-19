70a539dddab843551fd74147467999583e0425c2
ah1096 <arh.10.96@gmail.com>
2023-04-19 13:30:47 +0000

    edit landingtext/brand statement

--- portfolio/src/Components/LandingPage.js
+++ portfolio/src/Components/LandingPage.js
@@ -11,13 +11,13 @@ export const LandingPage = () => {
             <div className="row align-items-center vh-100">
 
                 <div id="landingtext" className="col-8">
-                <h1>Hi, I'm Alyssa</h1>
-                <p>a full stack web developer with a passion for creating engaging user experiences and elegant applications.</p>
-                <p>I'm currently searching for new opportunities.</p>
+                    <h1>Hi, I'm Alyssa</h1>
+                    <p>a full stack developer with a passion for creating engaging user experiences and elegant applications.</p>
+                    <p>Currently searching for new opportunities.</p>
                 </div>
 
                 <div id="landingicon" className="col-4">
-                <img className="w-75" src={brandLogo.imageUrl} alt="a dark pink hexagon inside with is Alyssa Holbert's brand logo, a small letter H inside of a large letter A; boths letters share a horizontal line" />
+                    <img className="w-100" src={brandLogo.imageUrl} alt="a dark pink hexagon inside with is Alyssa Holbert's brand logo, a small letter H inside of a large letter A; boths letters share a horizontal line" />
                 </div>
             </div>
         </div>
