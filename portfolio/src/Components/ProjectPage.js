d8cbb27963b450a08e36cbc65e6cfbc824ff4d38
ah1096 <arh.10.96@gmail.com>
2023-04-19 15:11:11 +0000

    add ProjectCard component

--- portfolio/src/Components/ProjectPage.js
+++ portfolio/src/Components/ProjectPage.js
@@ -1,3 +1,5 @@
+import { ProjectCard } from './ProjectCard.js'
+
 export const ProjectPage = () => {
     return (
     <div id="projectpage" className="container vh-100 w-100 mx-auto">
@@ -16,15 +18,7 @@ export const ProjectPage = () => {
         <div id="projectcards" className="h-75 row align-items-center">
 
         <div className="col-4 h-50">
-            <div className="card">
-                <div className="card-body">
-                    <h5 className="card-title">Project title</h5>
-                    <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <button type="button" className="btn btn-primary" href="#">demo</button>
-                    <button type="button" className="btn btn-primary" href="#">github</button>
-                </div>
-            </div>
+            <ProjectCard />
         </div>
 
         <div className="col-4">
