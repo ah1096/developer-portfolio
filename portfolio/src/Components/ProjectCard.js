96f310ece939d3801bf3de387bed4b9b3de32942
ah1096 <arh.10.96@gmail.com>
2023-04-19 17:27:42 +0000

    card styling; giving up for now bc can't center ul

--- portfolio/src/Components/ProjectCard.js
+++ portfolio/src/Components/ProjectCard.js
@@ -1,13 +1,42 @@
+import him from '../images/projects/him.jpg'
+const placeholder = {
+    name: 'placeholder',
+    imageUrl: him,
+};
 
 export const ProjectCard = () => {
     return(
-        <div className="card">
-            <div className="card-body">
-                <h5 className="card-title">Project title</h5>
-                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <button type="button" className="btn btn-primary" href="#">demo</button>
-                <button type="button" className="btn btn-primary" href="#">github</button>
+        <div>
+            <div>
+            <div class="hovercontainer h-100 w-100">
+                <img src={placeholder.imageUrl} className="projectimage h-100 w-100"/>
+                <div class="overlay">
+                    <div class="hovertext">
+                        <div id="projectaboutrow">
+                            <h5 id="project-title">Project title</h5>
+                            <h10 id="project-info" className="mb-2">Card subtitle</h10>
+                        </div>
+
+                        <div classname="row toolsused">
+                            <ul>
+                                <li>tool 1</li>
+                                <li>tool 2</li>
+                                <li>tool 3</li>
+                            </ul>
+                        </div>
+
+                        <div className="row" id="buttonrow">
+                            <div className="col">
+                                <button type="button" className="btn btn-primary pinkbutton" href="#">demo</button>
+                            </div>
+                            <div className="col">
+                                <button type="button" className="btn btn-primary pinkbutton" href="#">repo</button>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
             </div>
         </div>
     );
